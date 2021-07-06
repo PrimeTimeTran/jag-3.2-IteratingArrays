ab7c6056e96399616eb3126c2f8b18ec7323e950
Loi Van Tran <datloiboi@gmail.com>
2021-07-06 10:33:26 +0700

    Made with love by PrimeTime

---
+++ README.md
@@ -0,0 +1,11 @@
+# 3.2 Array Methods
+
+Goal for today.
+
+1. Iterators both good & bad.
+
+Make it to production
+
+Skills involved
+
+HTML, CSS, JS, Git
---
+++ programmers.js
@@ -0,0 +1,119 @@
+const programmers = [
+  "Dennis Ritchie",
+  "Brian Kernighan",
+  "Ken Thompson",
+  "Linus Torvalds",
+  "Bjarne Stroustrup",
+  "Tim Berners-Lee",
+  "Donald Knuth",
+  "Alan Turing",
+  "Mark Zuckerberg",
+  "Bill Gates",
+  "Larry Page",
+  "Elon Musk",
+  "Jack Dorsey",
+  "Satoshi Nakamoto",
+  "Ada Lovelace",
+  "Grace Hopper",
+  "Dan Ambramov",
+  "Jordan Walke",
+  "Ryan Dahl",
+  "David Heinemeier Hansson",
+  "Guido van Rossum",
+  "Yukihiro Matsumoto",
+  "Sergey Brin",
+  "Lyndsey Scott",
+  "Abhinav Asthana",
+  "Abhijit Kane",
+  "Ankit Sobti",
+  "Loi Tran",
+  "Charles Lee",
+  "Tuan Nguyen",
+  "Tan Vo",
+];
+
+
+function threePointOne() {
+  // 3.1
+  function printFirstName(element) {
+    console.log("printFirstName", element);
+    return element.split(" ")[0];
+  }
+
+  // Produces new array.
+  const newProgrammers = programmers.map(printFirstName);
+
+  document.getElementById("3.1").innerHTML = newProgrammers.join(" ");
+}
+
+threePointOne()
+function threePointOne() {
+  // 3.1
+  function printFirstName(element) {
+    console.log("printFirstName", element);
+    return element.split(" ")[0];
+  }
+
+  // Produces new array.
+  const newProgrammers = programmers.map(printFirstName);
+
+  document.getElementById("3.1").innerHTML = newProgrammers.join(" ");
+}
+
+threePointOne()
+function threePointOne() {
+  // 3.1
+  function printFirstName(element) {
+    console.log("printFirstName", element);
+    return element.split(" ")[0];
+  }
+
+  // Produces new array.
+  const newProgrammers = programmers.map(printFirstName);
+
+  document.getElementById("3.1").innerHTML = newProgrammers.join(" ");
+}
+
+threePointOne()
+function threePointOne() {
+  // 3.1
+  function printFirstName(element) {
+    console.log("printFirstName", element);
+    return element.split(" ")[0];
+  }
+
+  // Produces new array.
+  const newProgrammers = programmers.map(printFirstName);
+
+  document.getElementById("3.1").innerHTML = newProgrammers.join(" ");
+}
+
+threePointOne()
+function threePointOne() {
+  // 3.1
+  function printFirstName(element) {
+    console.log("printFirstName", element);
+    return element.split(" ")[0];
+  }
+
+  // Produces new array.
+  const newProgrammers = programmers.map(printFirstName);
+
+  document.getElementById("3.1").innerHTML = newProgrammers.join(" ");
+}
+
+threePointOne()
+function threePointOne() {
+  // 3.1
+  function printFirstName(element) {
+    console.log("printFirstName", element);
+    return element.split(" ")[0];
+  }
+
+  // Produces new array.
+  const newProgrammers = programmers.map(printFirstName);
+
+  document.getElementById("3.1").innerHTML = newProgrammers.join(" ");
+}
+
+threePointOne()
